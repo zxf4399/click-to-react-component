0f6eff30f3ea65cd6c25e9f888af2429f9b6e384
zxf4399 <zxf4399@gmail.com>
2022-10-08 17:16:00 +0800

    refactor: EventSource addEventListener logic

--- server/entry/content-script-auto-reload.js
+++ server/entry/content-script-auto-reload.js
@@ -1,4 +1,5 @@
 const { MESSAGE_FROM, MESSAGE_ACTION } = require("../constants")
+const log = require("logger").default.getLogger("content-script-auto-reload")
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   const shouldReload =
@@ -10,5 +11,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       action: MESSAGE_ACTION.RELOAD_EXTENSION,
       from: MESSAGE_FROM.CONTENT_SCRIPT,
     })
+
+    // wait for the background script to reload the extension
+    setTimeout(() => {
+      log.info("reload content script")
+      window.location.reload()
+    }, 100)
   }
 })
